a10541483cc91952c26e900ae9e6153729273948
Phatchara Champathong <Phatchara.Champathong@elev.ga.ntig.se>
2023-09-11 09:06:30 +0200

    liukjbkjubhö

---
+++ Arbete_Story 1/Program.cs
@@ -0,0 +1,19 @@
+﻿string? namn;
+
+Console.WriteLine("Du gick in i huset");
+Thread.Sleep(2000);
+Console.WriteLine("Vad heter du?")
+namn = Console.ReadLine();
+
+if (namn == "micke") 
+{
+    Console.WriteLine("Välkomen!");
+}
+else if (namn != "")
+{
+    Console.WriteLine("Vem fan är du!?");
+}
+
+
+
+Console.ReadKey();
